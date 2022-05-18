fe4f060aba9f69a97b0be405ab5d13d04d814cc6
sebranchett <s.e.branchett@tudelft.nl>
2022-05-18 14:21:28 +0200

    docker example now working

--- docker_example_configure.sh
+++ docker_example_configure.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 # Use this to install software packages
 sudo apt-get update -y
-echo 'docker here   ------------' > ~/output.txt
-which docker >> ~/output.txt
-echo 's3 buckets    ------------' >> ~/output.txt
-aws s3 ls >> ~/output.txt
-echo '--------------------------' >> ~/output.txt
+mkdir /tmp/home
+echo 'docker here   ------------' > /tmp/home/output.txt
+which docker >> /tmp/home/output.txt
+echo 's3 buckets    ------------' >> /tmp/home/output.txt
+aws s3 ls s3://seb-results-of-calculations >> /tmp/home/output.txt
+echo '--------------------------' >> /tmp/home/output.txt
 
-cd ~
+cd /tmp/home
+echo 'current dir   ------------' >> /tmp/home/output.txt
+pwd >> /tmp/home/output.txt
 aws s3 cp s3://seb-results-of-calculations/docker_tar_image.tar .
-echo 'files in home ------------' >> ~/output.txt
-ls -l >> ~/output.txt
+echo 'files in home ------------' >> /tmp/home/output.txt
+ls -l >> /tmp/home/output.txt
 sudo docker load < docker_tar_image.tar
-echo 'docker images ------------' >> ~/output.txt
-sudo docker image ls >> ~/output.txt
-echo '--------------------------' >> ~/output.txt
+echo 'docker images ------------' >> /tmp/home/output.txt
+sudo docker image ls >> /tmp/home/output.txt
+echo '--------------------------' >> /tmp/home/output.txt
 
-echo 'run test      ------------' >> ~/output.txt
-sudo docker run --rm sebranchett/gpaw:one_processor_21.1.0 gpaw test >> ~/output.txt
-echo '--------------------------' >> ~/output.txt
+echo 'run test      ------------' >> /tmp/home/output.txt
+sudo docker run --rm sebranchett/gpaw:one_processor_21.1.0 gpaw test >> /tmp/home/output.txt
+echo '--------------------------' >> /tmp/home/output.txt
 
-aws s3 cp ~/output.txt s3://seb-results-of-calculations
+aws s3 cp /tmp/home/output.txt s3://seb-results-of-calculations
 sudo shutdown -h now
