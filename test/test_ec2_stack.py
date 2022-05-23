06f4dfa3368d07e61e6920d3383613734270a570
sebranchett <s.e.branchett@tudelft.nl>
2022-05-23 17:41:25 +0200

    test vpc

--- test/test_ec2_stack.py
+++ test/test_ec2_stack.py
@@ -5,14 +5,28 @@ from aws_cdk.assertions import Template
 
 from app.ec2_instance_stack import EC2InstanceStack
 
+app = App()
+ec2_stack = EC2InstanceStack(app, "ec2-instance", env=Environment(
+                account="123456789012", region="eu-central-1"))
+
+# Prepare the stack for assertions.
+template = Template.from_stack(ec2_stack)
+
 
 def test_synthesizes_properly():
-    app = App()
-    ec2_stack = EC2InstanceStack(app, "ec2-instance", env=Environment(
-                    account="123456789012", region="eu-central-1"))
-
-    # Prepare the stack for assertions.
-    template = Template.from_stack(ec2_stack)
     template.resource_count_is(type="AWS::EC2::Instance", count=1)
     template.resource_count_is(type="AWS::EC2::Subnet", count=3)
     template.resource_count_is(type="AWS::IAM::Policy", count=1)
+
+
+def test_vpc():
+    template.has_resource_properties(
+        type="AWS::EC2::VPC",
+        props={"CidrBlock": "10.0.0.0/16",
+               "EnableDnsHostnames": True,
+               "EnableDnsSupport": True,
+               "InstanceTenancy": "default",
+               "Tags": [{"Key": "Name",
+                         "Value": "ec2-instance/VPC"}]
+               }
+    )
