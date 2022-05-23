1ff726782c107e96701aafeee55d9501e54260ce
sebranchett <s.e.branchett@tudelft.nl>
2022-05-23 16:52:22 +0200

    added first test

---
+++ test/test_ec2_stack.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from aws_cdk import App, Environment
+from aws_cdk.assertions import Template
+# from aws_cdk.assertions import Capture, Match
+
+from app.ec2_instance_stack import EC2InstanceStack
+
+
+def test_synthesizes_properly():
+    app = App()
+    ec2_stack = EC2InstanceStack(app, "ec2-instance", env=Environment(
+                    account="123456789012", region="eu-central-1"))
+
+    # Prepare the stack for assertions.
+    template = Template.from_stack(ec2_stack)
+    template.resource_count_is(type="AWS::EC2::Instance", count=1)
