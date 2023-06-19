57eed0c941e3db2abf65624bf8ced866f26442aa
sebranchett <s.e.branchett@tudelft.nl>
2023-06-19 17:34:22 +0200

    run PyTorch example as part of set-up

--- startup.sh
+++ startup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Use this to install software packages
+if VERB="$( which apt-get )" 2> /dev/null; then
+   sudo apt-get update -y
+elif VERB="$( which yum )" 2> /dev/null; then
+   sudo yum update -y
+else
+   echo "WARNING: Could not update the system" >&2
+fi
 
 # Install SSM agent, so you can log in. This us the Ubuntu version
 # See https://repost.aws/knowledge-center/install-ssm-agent-ec2-linux#
@@ -8,4 +16,13 @@ wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64
 sudo dpkg -i amazon-ssm-agent.deb
 sudo systemctl enable amazon-ssm-agent
 
+# Clone a PyTorch example
+cd /tmp
+git clone https://github.com/sebranchett/delftblue-gpu-apptainer-pytorch.git
+cd delftblue-gpu-apptainer-pytorch
+
+# Run the PyTorch example. Be patient, it takes a while. The VM will enter Stopped state on completion
+sudo docker run --gpus all --rm -v "$PWD":/workspace/test nvcr.io/nvidia/pytorch:22.10-py3 /bin/sh -c "cd test;python < quickstart.py > quickstart.log"
+
+aws s3 cp quickstart.log s3://tudelft-results-of-calculations
 sudo shutdown -h now
