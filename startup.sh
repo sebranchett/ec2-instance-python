4d5d8a56743204757520d69d57b944d1e400713f
sebranchett <s.e.branchett@tudelft.nl>
2023-06-19 15:52:16 +0200

    install SSM so you can log in to VM

--- startup.sh
+++ startup.sh
@@ -1,23 +1,11 @@
-#!/bin/sh
-# Use this to install software packages
-if VERB="$( which apt-get )" 2> /dev/null; then
-   sudo apt-get update -y
-elif VERB="$( which yum )" 2> /dev/null; then
-   sudo yum update -y
-else
-   echo "WARNING: Could not update the system" >&2
-fi
+#!/bin/bash
 
-echo 'docker here   ------------' > /tmp/output.txt
-which docker >> /tmp/output.txt
-echo 's3 buckets    ------------' >> /tmp/output.txt
-aws s3 ls s3://tudelft-results-of-calculations >> /tmp/output.txt
-echo 'who am I      ------------' >> /tmp/output.txt
-whoami >> /tmp/output.txt
-echo 'current dir   ------------' >> /tmp/output.txt
-pwd >> /tmp/output.txt
-echo 'files in dir  ------------' >> /tmp/output.txt
-ls -l >> /tmp/output.txt
+# Install SSM agent, so you can log in. This us the Ubuntu version
+# See https://repost.aws/knowledge-center/install-ssm-agent-ec2-linux#
+mkdir /tmp/ssm
+cd /tmp/ssm
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
 
-aws s3 cp /tmp/output.txt s3://tudelft-results-of-calculations
 sudo shutdown -h now
